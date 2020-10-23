2f3a9866c0ba7aa64178f403854e903b970927ea
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-10-23 23:37:58 +0200

    Initial commit

---
+++ test/dummy.js
@@ -0,0 +1,7 @@
+const { expect, assert } = require('chai');
+
+describe('Dummy test for CircleCI integration', function() {
+    it('should succeed', function() {
+        expect(true).to.be.true;
+    })
+})
