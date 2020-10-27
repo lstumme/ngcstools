587400294a791d932e21059ee9d01392fd31c104
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-10-27 23:00:25 +0100

    Adding createTool controller function

--- controllers/toolcontroller.js
+++ controllers/toolcontroller.js
@@ -1,35 +1,51 @@
 const toolservices = require('../services/toolservices.js');
 
-exports.createTool = (req, res, next) => {
+exports.createTool = async (req, res, next) => {
+    const name = req.body.name;
+    if (!name) {
+        const error = new Error('Bad arguments');
+        error.statusCode = 400;
+        throw error;
+    }
+    return toolservices.createTool({ name })
+        .then(response => {
+            res.status(201).json({ ...response });
+            return response;
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.deleteTool = async (req, res, next) => {
 
 };
 
-exports.deleteTool = (req, res, next) => {
+exports.updateToolInformations = async (req, res, next) => {
 
 };
 
-exports.updateToolInformations = (req, res, next) => {
+exports.getTool = async (req, res, next) => {
 
 };
 
-exports.getTool = (req, res, next) => {
+exports.getTools = async (req, res, next) => {
 
 };
 
-exports.getTools = (req, res,next) => {
+exports.createToolVersion = async (req, res, next) => {
 
 };
-
-exports.createToolVersion = (req, res, next) => {
-
-};
-exports.deleteToolVersion = (req, res, next) => {
+exports.deleteToolVersion = async (req, res, next) => {
 
 };
-exports.getToolVersion = (req, res, next) => {
+exports.getToolVersion = async (req, res, next) => {
 
 };
-exports.getToolVersions = (req, res, next) => {
+exports.getToolVersions = async (req, res, next) => {
 
 };
 
--- test/tool-controller.js
+++ test/tool-controller.js
@@ -1,33 +1,129 @@
 const { expect, assert } = require('chai');
+const sinon = require('sinon');
 const dbHandler = require('./db-handler');
+const toolController = require('../controllers/toolcontroller');
+const toolServices = require('../services/toolservices');
+
 
 describe('Tool Controller', function () {
-    describe('#createTool function', function() {
+    describe('#createTool function', function () {
+        beforeEach(function () {
+            sinon.stub(toolServices, 'createTool');
+        })
+
+        afterEach(function () {
+            toolServices.createTool.restore();
+        });
+
+        it('should throw an Error if name is not specified', function (done) {
+            const req = {
+                body: {}
+            };
+            toolController.createTool(req, {}, () => { })
+                .then(response => {
+                    assert.fail('createTool Error');
+                })
+                .catch(err => {
+                    expect(err).to.have.property('statusCode', 400);
+                    done();
+                })
+        });
+
+        it('should return an object if tool creation succeed', function (done) {
+            const req = {
+                body: { name: 'tool1' }
+            }
+            const res = {
+                statusCode: 0,
+                jsonObject: {},
+                status: function (code) {
+                    this.statusCode = code;
+                    return this;
+                },
+                json: function (value) {
+                    this.jsonObject = value;
+                    return this;
+                }
+            };
+
+            toolServices.createTool.returns(new Promise((resolve, reject) => {
+                resolve({ toolId: 'abcd', name: 'tool1' });
+            }));
+
+            toolController.createTool(req, res, () => { })
+                .then(result => {
+                    expect(res).to.have.property('statusCode', 201);
+                    expect(res.jsonObject).to.have.property('toolId', 'abcd');
+                    expect(res.jsonObject).to.have.property('name', 'tool1');
+                    done();
+                })
+        });
+
+        it('should call next(err) adding default statusCode if not specified', function (done) {
+            const req = {
+                body: { name: 'tool1' }
+            }
+
+            toolServices.createTool.returns(new Promise((resolve, reject) => {
+                reject(new Error('tool Service error'));
+            }));
+
+            let error = null;
+            const next = (err) => {
+                error = err;
+            };
+            toolController.createTool(req, {}, next).then(result => {
+                expect(error).to.not.be.null;
+                expect(error).to.have.property('statusCode', 500);
+                done();
+            });
+        });
+
+        it('should call next(err) keeping specified statusCode', function (done) {
+            const req = {
+                body: { name: 'tool1' }
+            }
+
+            toolServices.createTool.returns(new Promise((resolve, reject) => {
+                const error = new Error('tool Service error');
+                error.statusCode = 400;
+                reject(error);
+            }));
+            let error = null;
+            const next = (err) => {
+                error = err;
+            }
+            toolController.createTool(req, {}, next).then(result => {
+                expect(error).to.not.be.null;
+                expect(error).to.have.property('statusCode', 400);
+                done();
+            });
+        });
+    });
+
+    describe('#deleteTool function', function () {
 
     });
-    describe('#deleteTool function', function() {
+    describe('#updateToolInformations function', function () {
 
     });
-    describe('#updateToolInformations function', function() {
+    describe('#getTool function', function () {
 
     });
-    describe('#getTool function', function() {
+    describe('#getTools function', function () {
 
     });
-    describe('#getTools function', function() {
+    describe('#createToolVersion function', function () {
 
     });
-    describe('#createToolVersion function', function() {
+    describe('#deleteToolVersion function', function () {
 
     });
-    describe('#deleteToolVersion function', function() {
+    describe('#getToolVersion function', function () {
 
     });
-    describe('#getToolVersion function', function() {
+    describe('#getToolVersions function', function () {
 
     });
-    describe('#getToolVersions function', function() {
 
-    });
-    
 });
