1fc0f5a2ad447ff9bbc0c812f991e589e42ba380
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-11-04 23:02:08 +0100

    Code correction

--- routes/environmentroutes.js
+++ routes/environmentroutes.js
@@ -6,10 +6,13 @@ const environmentController = require('../controllers/environmentcontroller');
 
 const initRouter = () => {
     const router = express.Router();
+
     router.post('/createEnvironment', isAuth, isToolManager, environmentController.createTool);
     router.delete('/deleteEnvironment', isAuth, isToolManager, environmentController.deleteTool);
     router.put('/updateEnvironmentInformations', isAuth, isToolManager, environmentController.updateToolInformations);
     router.get('/getEnvironment:environmentId', isAuth, environmentController.getTool);
     router.get('/getEnvironments', isAuth, environmentController.getTools);
+
+    return router;
 };
 module.exports = initRouter;
